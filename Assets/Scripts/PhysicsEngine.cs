e04079e69f1f41298c0271379f953be6ca3a7dda
Serkan Şenkal <serkansenkal97@gmail.com>
2021-03-08 10:22:44 +0300

    L11

---
+++ Assets/Scripts/PhysicsEngine.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsEngine : MonoBehaviour
+{
+    public float mass = 1f;
+    public Vector3 velocityVector = Vector3.zero; //Avg. velocity this FixedUpdate()
+    public Vector3 netForceVector = Vector3.zero;
+    public List<Vector3> forceVectorList = new List<Vector3>();
+
+    private void FixedUpdate()
+    {
+        AddForce();
+
+        UpdateVelocity();
+
+        UpdatePosition();
+    }
+
+    private void AddForce()
+    {
+        netForceVector = Vector3.zero;
+
+        foreach (Vector3 _forceVector in forceVectorList)
+        {
+            netForceVector += _forceVector;
+        }
+    }
+
+    private void UpdateVelocity()
+    {
+        Vector3 _accelerationVector = netForceVector / mass;
+
+        velocityVector += _accelerationVector * Time.deltaTime;
+    }
+
+    private void UpdatePosition()
+    {
+        Vector3 _deltaS = velocityVector * Time.deltaTime;
+
+        transform.position += _deltaS;
+    }
+}
